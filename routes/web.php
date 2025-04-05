d3168cd73a099f38824a297a7b8a4441a549162d
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-04-05 22:44:29 +0500

    feat: update welcome route to return JSON response

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Welcome to GOAT',
+        'status' => 200,
+    ]);
 });
