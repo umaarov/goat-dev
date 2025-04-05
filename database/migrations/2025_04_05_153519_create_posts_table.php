77bb4efec9e9f06c8dadc761d15740b8bbd10070
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-04-05 20:40:01 +0500

    migrations

---
+++ database/migrations/2025_04_05_153519_create_posts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('question');
+            $table->string('option_one_title');
+            $table->string('option_one_image')->nullable();
+            $table->string('option_two_title');
+            $table->string('option_two_image')->nullable();
+            $table->integer('option_one_votes')->default(0);
+            $table->integer('option_two_votes')->default(0);
+            $table->integer('total_votes')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
