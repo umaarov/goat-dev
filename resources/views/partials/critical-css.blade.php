45ab43f2e5fffc0c14844a9c5ea0c6d99ef888f0
umaarov <hs.umarov21@gmail.com>
2025-06-17 20:26:35 +0500

    refactor: revert critical CSS implementation and restore original stylesheet loading method

--- resources/views/partials/critical-css.blade.php
+++
@@ -1,3 +0,0 @@
-<style>
-    @import url(https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap);@import url(https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap);
-</style>
