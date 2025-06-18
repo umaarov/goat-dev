25fab9f2189a05434b6dfe05ecd9bc743bafc7f1
umaarov <hs.umarov21@gmail.com>
2025-06-18 09:42:27 +0500

    fix: handle streamed responses in SecurityHeaders middleware

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -3,12 +3,16 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class SecurityHeaders
 {
     public function handle($request, Closure $next)
     {
         $response = $next($request);
+        if ($response instanceof StreamedResponse) {
+            return $response;
+        }
         $response->header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
         $response->header('X-Frame-Options', 'SAMEORIGIN');
         $response->header('X-Content-Type-Options', 'nosniff');
