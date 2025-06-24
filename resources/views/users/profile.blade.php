b9a17960a541f6f08046f4fa441cd9a7f8c1d1e0
umaarov <hs.umarov21@gmail.com>
2025-06-24 13:09:39 +0500

    refactor: reorganize ratings tab display in user profile

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -62,14 +62,6 @@
                                 <span class="font-semibold text-gray-800">{{ number_format($totalVotesOnUserPosts) }}</span>
                                 <span class="text-gray-500">{{ trans_choice('messages.profile.votes_collected_stat_label', $totalVotesOnUserPosts) }}</span>
                             </div>
-                            <div>
-                                <a href="{{ route('rating.index') }}" title="{{ __('messages.ratings.title') }}" class="flex items-center text-gray-600 hover:text-blue-700 font-medium transition-colors duration-150">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1 text-amber-500" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                                    </svg>
-                                    <span>{{ __('messages.ratings.nav_title') }}</span>
-                                </a>
-                            </div>
                         </div>
 
                         {{-- External Links Section --}}
@@ -156,6 +148,15 @@
                           id="voted-posts-indicator"></span>
                 </button>
             @endif
+
+            {{-- Ratings Tab --}}
+            <a href="{{ route('rating.index') }}"
+               class="px-6 py-3 font-medium text-gray-700 hover:text-blue-800 focus:outline-none relative flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1.5 text-amber-500" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <span>{{ __('messages.ratings.nav_title') }}</span>
+            </a>
         </div>
     </div>
 
