f004daac9a8d6d121beeabe199d30408c7da3c2f
umaarov <hs.umarov21@gmail.com>
2025-06-24 11:28:45 +0500

    refactor: optimize queries in RatingController

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -14,23 +14,10 @@ class RatingController extends Controller
         $limit = 25;
 
         // Most votes collected on posts
-        $topByPostVotes = User::query()
-            ->select([
-                'users.id',
-                'users.first_name',
-                'users.last_name',
-                'users.username',
-                'users.profile_picture',
-                DB::raw('SUM(posts.total_votes) as total_post_votes')
-            ])
+        $topByPostVotes = DB::table('users')
+            ->select('users.id', 'users.first_name', 'users.last_name', 'users.username', 'users.profile_picture', DB::raw('SUM(posts.total_votes) as total_post_votes'))
             ->join('posts', 'users.id', '=', 'posts.user_id')
-            ->groupBy(
-                'users.id',
-                'users.first_name',
-                'users.last_name',
-                'users.username',
-                'users.profile_picture'
-            )
+            ->groupBy('users.id', 'users.first_name', 'users.last_name', 'users.username', 'users.profile_picture')
             ->orderByDesc('total_post_votes')
             ->take($limit)
             ->get();
@@ -39,6 +26,7 @@ class RatingController extends Controller
         $topByPostCount = User::query()
             ->withCount('posts')
             ->orderByDesc('posts_count')
+            ->where('posts_count', '>', 0)
             ->take($limit)
             ->get();
 
@@ -46,32 +34,19 @@ class RatingController extends Controller
         $topByCommentCount = User::query()
             ->withCount('comments')
             ->orderByDesc('comments_count')
+            ->where('comments_count', '>', 0)
             ->take($limit)
             ->get();
 
         // Most likes collected on comments
-        $topByCommentLikes = User::query()
-            ->select([
-                'users.id',
-                'users.first_name',
-                'users.last_name',
-                'users.username',
-                'users.profile_picture',
-                DB::raw('SUM(comments.likes_count) as total_comment_likes')
-            ])
+        $topByCommentLikes = DB::table('users')
+            ->select('users.id', 'users.first_name', 'users.last_name', 'users.username', 'users.profile_picture', DB::raw('SUM(comments.likes_count) as total_comment_likes'))
             ->join('comments', 'users.id', '=', 'comments.user_id')
             ->where('comments.likes_count', '>', 0)
-            ->groupBy(
-                'users.id',
-                'users.first_name',
-                'users.last_name',
-                'users.username',
-                'users.profile_picture'
-            )
+            ->groupBy('users.id', 'users.first_name', 'users.last_name', 'users.username', 'users.profile_picture')
             ->orderByDesc('total_comment_likes')
             ->take($limit)
             ->get();
-
 
         return view('rating.index', compact(
             'topByPostVotes',
