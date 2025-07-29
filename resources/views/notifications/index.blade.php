d512997000698734cbd4fa2d71baee0c3af1b58a
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-07-29 15:36:59 +0500

    fix(notification) old url method

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -6,7 +6,7 @@
     <div class="container mb-4 mx-auto px-4">
 
         <div class="max-w-3xl mx-auto">
-{{--            <h1 class="text-2xl font-bold text-gray-800 mb-6">{{ __('messages.notifications.page_title') ?? 'Notifications' }}</h1>--}}
+            {{--            <h1 class="text-2xl font-bold text-gray-800 mb-6">{{ __('messages.notifications.page_title') ?? 'Notifications' }}</h1>--}}
 
             <div class="bg-white rounded-lg shadow-[inset_0_0_0_0.5px_rgba(0,0,0,0.2)]">
                 <ul class="divide-y divide-gray-200">
@@ -19,26 +19,31 @@
 
                                 $commentId = $data['comment_id'] ?? $data['reply_id'] ?? 0;
 
-                                $postUrl = route('posts.showSlug', [
-                                    'id' => $data['post_id'] ?? 0,
-                                    'slug' => 'post',
-                                    'comment' => $commentId
-                                ]);
+                                $postUrl = route('posts.show', [
+                                    'post' => $data['post_id'] ?? 0,
+                                    '#comment-' . $commentId
+                                    ]);
                             @endphp
 
                             <div class="flex items-start space-x-4">
                                 <div class="flex-shrink-0">
                                     @if($notificationType === 'CommentLiked')
                                         <svg class="h-6 w-6 text-red-500" fill="currentColor" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"></path>
+                                            <path fill-rule="evenodd"
+                                                  d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                                                  clip-rule="evenodd"></path>
                                         </svg>
                                     @elseif($notificationType === 'NewReplyToYourComment')
-                                        <svg class="h-6 w-6 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 016 6v3" />
+                                        <svg class="h-6 w-6 text-blue-500" xmlns="http://www.w3.org/2000/svg"
+                                             fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                  d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 016 6v3"/>
                                         </svg>
                                     @elseif($notificationType === 'YouWereMentioned')
-                                        <svg class="h-6 w-6 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path>
+                                        <svg class="h-6 w-6 text-green-500" fill="none" stroke="currentColor"
+                                             viewBox="0 0 24 24">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path>
                                         </svg>
                                     @endif
                                 </div>
@@ -46,13 +51,16 @@
                                 <div class="flex-grow">
                                     <p class="text-sm text-gray-800">
                                         @if($notificationType === 'CommentLiked')
-                                            <a href="{{ route('profile.show', $data['liker_name']) }}" class="font-bold hover:underline">{{ $data['liker_name'] }}</a>
+                                            <a href="{{ route('profile.show', $data['liker_name']) }}"
+                                               class="font-bold hover:underline">{{ $data['liker_name'] }}</a>
                                             liked your comment: "{{ $data['comment_content'] }}"
                                         @elseif($notificationType === 'NewReplyToYourComment')
-                                            <a href="{{ route('profile.show', $data['replier_name']) }}" class="font-bold hover:underline">{{ $data['replier_name'] }}</a>
+                                            <a href="{{ route('profile.show', $data['replier_name']) }}"
+                                               class="font-bold hover:underline">{{ $data['replier_name'] }}</a>
                                             replied to your comment: "{{ $data['reply_content'] }}"
                                         @elseif($notificationType === 'YouWereMentioned')
-                                            <a href="{{ route('profile.show', $data['mentioner_name']) }}" class="font-bold hover:underline">{{ $data['mentioner_name'] }}</a>
+                                            <a href="{{ route('profile.show', $data['mentioner_name']) }}"
+                                               class="font-bold hover:underline">{{ $data['mentioner_name'] }}</a>
                                             mentioned you in a comment: "{{ $data['comment_content'] }}"
                                         @endif
                                     </p>
