729aa1c46ac4933d9025803e5b5ab9af8cb26bd4
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-07-30 15:27:43 +0500

    fix(trust-proxies): configure trusted headers for Cloudflare support

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,8 +3,15 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Symfony\Component\HttpFoundation\Request as RequestAlias;
 
 class TrustProxies extends Middleware
 {
-    protected array|string|null $proxies = '*';
+    protected $proxies = '*';
+
+    protected $headers =
+        RequestAlias::HEADER_X_FORWARDED_FOR |
+        RequestAlias::HEADER_X_FORWARDED_HOST |
+        RequestAlias::HEADER_X_FORWARDED_PORT |
+        RequestAlias::HEADER_X_FORWARDED_PROTO;
 }
