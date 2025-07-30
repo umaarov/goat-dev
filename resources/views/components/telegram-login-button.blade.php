6f669a49aa4620b4e385e2b06347775d6d905a02
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-07-30 12:23:59 +0500

    merge telegram login widget with custom login button

--- resources/views/components/telegram-login-button.blade.php
+++ resources/views/components/telegram-login-button.blade.php
@@ -7,30 +7,32 @@
     <div class="mb-2">
         <div id="telegram-login-button-container"
              class="w-full flex items-center justify-center bg-[#2AABEE] text-white py-2 rounded-md hover:bg-[#1E98D4] focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer">
-            <svg class="w-5 h-5 mr-2 flex-shrink-0" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+            <svg class="h-5 w-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
                 <path
-                    d="M9.78 18.65l.28-4.23 7.68-6.92c.34-.31-.07-.46-.52-.19L7.74 13.3 3.64 12c-.88-.25-.89-.86.2-1.3l15.97-6.16c.73-.33 1.43.18 1.15 1.3l-2.72 12.57c-.28 1.13-1.04 1.4-1.74.88L14.25 16l-4.12 3.9c-.78.76-1.36.37-1.57-.49z"/>
+                    d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.962 6.502-1.36 8.627-.17.9-.502 1.203-1.008 1.23-1.08.056-1.895-.72-2.968-1.42-1.492-.99-2.32-1.604-3.8-2.593-.284-.188-.506-.347-.78-.71-.038-.048-.07-.092-.102-.132-.383-.49-.284-1.01.213-1.528L13.87 8.32a.506.506 0 0 1 .497-.288c.283.006.471.144.536.216l-2.147 4.957-1.16-3.419a.488.488 0 0 1 .15-.497c.18-.134.4-.188.602-.182.22 0 .42.06.636.148l4.34 2.158.465-4.354z"/>
             </svg>
             <span>{{ __('messages.auth.login_with_telegram') }}</span>
         </div>
     </div>
 
-    <script async src="https://telegram.org/js/telegram-widget.js?22"
-            data-telegram-login="{{ $botUsername }}"
-            data-size="large"
-            data-auth-url="{{ $callbackRoute }}"
-            data-request-access="write"
-            onload="
-            var container = document.getElementById('telegram-login-button-container');
-            var script = document.querySelector('script[data-telegram-login]');
-            var iframe = script.nextSibling;
-            if (iframe && iframe.tagName === 'IFRAME') {
-                container.appendChild(iframe);
-                iframe.style.width = '100%';
-                iframe.style.height = '42px';
-            }
-        "
-    ></script>
+    <div id="telegram-script-wrapper" style="display: none;">
+        <script async src="https://telegram.org/js/telegram-widget.js?22"
+                data-telegram-login="{{ $botUsername }}"
+                data-size="large"
+                data-auth-url="{{ $callbackRoute }}"
+                data-request-access="write"
+                onload="
+                    var container = document.getElementById('telegram-login-button-container');
+                    var scriptWrapper = document.getElementById('telegram-script-wrapper');
+
+                    var iframe = scriptWrapper.querySelector('iframe');
+
+                    if (iframe) {
+                        container.appendChild(iframe);
+                    }
+                "
+        ></script>
+    </div>
 
     <style>
         #telegram-login-button-container iframe {
