05c52e06d654f99f64c053fcb67a72166d9ac548
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-07-30 16:08:59 +0500

    handle exceptions with custom views (st)

--- routes/web.php
+++ routes/web.php
@@ -164,20 +164,3 @@ Route::get('/__netdebug', function (): JsonResponse {
         'scheme'          => request()->getScheme(),
     ]);
 });
-
-Route::get('/test-403', function () {
-    abort(403);
-});
-
-Route::get('/test-401', function () {
-    abort(401);
-});
-
-Route::get('/test-429', function () {
-    abort(429);
-});
-
-Route::get('/test-500', function () {
-    throw new Exception("Simulated server error");
-});
-
