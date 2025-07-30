c77834bbc54adf2e7eeb2d3060534e543f7f03de
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-07-30 15:33:12 +0500

    Configure trusted proxies for Cloudflare with explicit forwarded headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Console\Commands\CleanupUnverifiedUsers;
 use App\Console\Commands\GenerateSitemap;
+use App\Console\Commands\SendPostNotifications;
 use App\Http\Middleware\EnsurePasswordIsSet;
 use App\Http\Middleware\SecurityHeaders;
 use App\Http\Middleware\SetLocale;
@@ -11,6 +12,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
 use Spatie\ResponseCache\Middlewares\CacheResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -32,12 +34,43 @@ return Application::configure(basePath: dirname(__DIR__))
             SetLocale::class,
 //            AddCspHeaders::class,
         ]);
+        $middleware->trustProxies(
+            at: [
+                '103.21.244.0/22',
+                '103.22.200.0/22',
+                '103.31.4.0/22',
+                '104.16.0.0/13',
+                '104.24.0.0/14',
+                '108.162.192.0/18',
+                '131.0.72.0/22',
+                '141.101.64.0/18',
+                '162.158.0.0/15',
+                '172.64.0.0/13',
+                '173.245.48.0/20',
+                '188.114.96.0/20',
+                '190.93.240.0/20',
+                '197.234.240.0/22',
+                '198.41.128.0/17',
+
+                '2400:cb00::/32',
+                '2606:4700::/32',
+                '2803:f800::/32',
+                '2405:b500::/32',
+                '2405:8100::/32',
+                '2a06:98c0::/29',
+                '2c0f:f248::/32',
+            ],
+            headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->withCommands(
         (array)CleanupUnverifiedUsers::class,
-        \App\Console\Commands\SendPostNotifications::class,
+        SendPostNotifications::class,
         GenerateSitemap::class
     )->withSchedule(
         function ($schedule) {
