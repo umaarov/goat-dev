3963b7018466441530473912c2e84c94a7b3f452
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-07-31 16:22:21 +0500

    move posturl to show.blade

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title', $post->question . ' - GOAT.uz')
 @section('meta_description', Str::limit($post->ai_generated_context ?? $post->question, 160))
-
+@php $postUrl = route('posts.show.user-scoped', ['username' => $post->user->username, 'post' => $post->id]); @endphp
 @push('schema')
     <script type="application/ld+json">
         {
