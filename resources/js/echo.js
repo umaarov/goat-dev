2e2159d2f859c2b8bd3e2bc567df187dfa2a9f5b
Ismoiljon Umarov <hs.umarov21@gmail.com>
2025-12-13 10:52:47 +0500

    Refactors Echo setup, making it conditional and adds Pusher env vars.
    Signed-off-by: Ismoiljon Umarov <hs.umarov21@gmail.com>

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,15 +1,22 @@
 import Echo from 'laravel-echo';
-
 import Pusher from 'pusher-js';
+
 window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true,
-    wsHost: import.meta.env.VITE_PUSHER_HOST,
-    wsPort: import.meta.env.VITE_PUSHER_PORT,
-    wssPort: import.meta.env.VITE_PUSHER_PORT,
-    enabledTransports: ['ws', 'wss'],
-});
+if (import.meta.env.VITE_PUSHER_APP_KEY) {
+    window.Echo = new Echo({
+        broadcaster: 'pusher',
+        key: import.meta.env.VITE_PUSHER_APP_KEY,
+        cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+        wsHost: import.meta.env.VITE_PUSHER_HOST || `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+        wsPort: import.meta.env.VITE_PUSHER_PORT || 80,
+        wssPort: import.meta.env.VITE_PUSHER_PORT || 443,
+        forceTLS: (import.meta.env.VITE_PUSHER_SCHEME === 'https'),
+        enabledTransports: ['ws', 'wss'],
+        encrypted: import.meta.env.VITE_PUSHER_APP_ENCRYPTED === 'true',
+        disableStats: true,
+    });
+} else {
+    console.warn('Pusher app key not found. Echo will not be initialized.');
+    window.Echo = null;
+}
