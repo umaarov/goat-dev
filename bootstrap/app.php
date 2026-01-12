8c6d2daa0c8d2172f6cfd859d4e16197a048c3d7
Ismoiljon Umarov <hs.umarov21@gmail.com>
2026-01-12 11:22:15 +0500

    Exempts refresh_token from cookie encryption.
    Signed-off-by: Ismoiljon Umarov <hs.umarov21@gmail.com>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,6 +29,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->encryptCookies(except: [
+            'refresh_token',
+        ]);
         $middleware->alias([
             'cache.response' => CacheResponse::class,
             'password.confirm' => RequirePassword::class,
